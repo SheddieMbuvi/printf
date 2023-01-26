e1892e851d9a8db965dceed7a5cc434fa17b8150
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-01-26 11:51:34 -0800

    Perfect one

--- printf.c
+++ printf.c
@@ -16,6 +16,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
+		if (format[i] == '\0')
+			return ('\n');
 		if (format[i] == '%')
 		{
 			i++;
