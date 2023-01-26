8e11021861b2a3573e36e78521024e5451334c7e
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-01-26 11:54:50 -0800

    Perfect one

--- printf.c
+++ printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '\0')
-			return ('\n');
+			return ('\0');
 		if (format[i] == '%')
 		{
 			i++;
