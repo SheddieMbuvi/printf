f010ec8e888fd7952eb923dbe33ed3a8468a88e7
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-01-26 03:09:14 -0800

    Task 1

--- printf.c
+++ printf.c
@@ -11,38 +11,41 @@
 int _printf(const char *format, ...)
 {
 	int count = 0;
-    va_list args;
-    va_start(args, format);
-
-    for (int i = 0; format[i] != '\0'; i++) {
-        if (format[i] == '%') {
-            i++;
-            switch (format[i]) {
-                case 'c':
-                    _putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    fputs(va_arg(args, char*), stdout);
-                    count += _strlen(va_arg(args, char*));
-                    break;
-                case '%':
-                    _putchar('%');
-                    count++;
-                    break;
-                default:
-                    _putchar(format[i]);
-                    count++;
-                    break;
-            }
-        }
-       else
-       {
-            _putchar(format[i]);
-            count++;
-       }
-    }
-    va_end(args);
-    return count;
+	va_list args;
+	
+	va_start(args, format);
+	for (int i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					fputs(va_arg(args, char*), stdout);
+					count += _strlen(va_arg(args, char*));
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar(format[i]);
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
 
